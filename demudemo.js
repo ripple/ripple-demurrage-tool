da55d0f37e85f1198c24591f9ab0fe40daccc777
mDuo13 <mduo13@gmail.com>
2015-01-30 13:18:52 -0800

    improved js code quality

--- demudemo.js
+++ demudemo.js
@@ -1,5 +1,6 @@
 // demurrage_demo.js - demonstrate 2-way demurrage/interest conversions
 // Requires: jQuery, ripple-lib
+/*global $:false, ripple:false */
 
 var ADDRESS_ONE = "rrrrrrrrrrrrrrrrrrrrBZbvji";
 var Amount = ripple.Amount;
@@ -21,15 +22,19 @@ $(document).ready(function() {
 });
 
 function calc_to_display() {
-    ref_time = get_ref_time();
-    curcode = gen_curcode();
-    ledger_amount = get_ledger_amount();
+    var ref_time = get_ref_time();
+    var curcode = gen_curcode();
+    var ledger_amount = get_ledger_amount();
     
-    demAmount = Amount.from_json(ledger_amount+"/"+curcode+"/"+ADDRESS_ONE);
+    if (ref_time === false || curcode === false || ledger_amount === false) {
+        return false;
+    }
+    
+    var demAmount = Amount.from_json(ledger_amount+"/"+curcode+"/"+ADDRESS_ONE);
     demAmount = demAmount.applyInterest(ref_time);
     
     if (demAmount === undefined ) {
-        alert("Error: ripple-lib failed to calculate interest. Maybe not a valid date?");
+        display_error("Error: ripple-lib failed to calculate interest. Maybe not a valid date?");
     }
     
     set_display_amount(demAmount.to_json().value);
@@ -40,7 +45,7 @@ function calc_to_ledger() {
     var curcode = gen_curcode();
     var display_value = get_display_amount();
     
-    ledgAmount =Amount.from_human(display_value+" "+curcode, {reference_date:ref_time});
+    var ledgAmount = Amount.from_human(display_value+" "+curcode, {reference_date:ref_time});
     //ledgAmount.set_issuer(ADDRESS_ONE);//this step seems unnecessary
     
     set_ledger_amount(ledgAmount.to_json().value);
@@ -48,8 +53,8 @@ function calc_to_ledger() {
 
 
 function gen_curcode() {
-    threeletters = $("#cur_1").val();
-    rate = $("#rate_1").val();
+    var threeletters = $("#cur_1").val();
+    var rate = $("#rate_1").val();
     
     //Workaround RLJS-188
     if (rate.indexOf(".") == -1) {
@@ -58,49 +63,47 @@ function gen_curcode() {
     
     var c = Currency.from_json(threeletters+" ("+rate+"%pa)");
     
-    if (c.is_valid()) {
-        if (c.is_native()) {
-            alert("Error: XRP cannot have interest or demurrage.");
-            return false;
-        } else {
-            $("#hex_1").val(c.to_hex());
-            $("#ledger_cur_1").text(c.to_json());
-            return c.to_hex();
-        }
-    } else {
-        $("#hex_1").val("(Invalid currency/rate)");
+    if (!c.is_valid()) {
+        display_error("Error: invalid currency/rate");
         return false;
+    } else if (c.is_native()) {
+        display_error("Error: XRP cannot have interest or demurrage.");
+        return false;
+    } else {
+        $("#hex_1").val(c.to_hex());
+        $("#ledger_cur_1").text(c.to_json());
+        return c.to_hex();
     }
+       
 }
 
 function parse_curcode() {
-    hex = $("#hex_1").val();
-    
+    var hex = $("#hex_1").val();
     var c = Currency.from_hex(hex);
+    var threeletters = "";
+    var rate = 0;
     
-    if (c.is_valid()) {
-        if (c.is_native()) {
-            alert("Error: XRP cannot have interest or demurrage.");
-            return false;
-        } else {
-            threeletters = c.get_iso();
-            $("#cur_1").val(threeletters);
-            
-            //Convert rate to percentage
-            rate = (c.get_interest_at(0) -1) * 100;
-            $("#rate_1").val(rate);
-            
-            //some currencies' to_human() output is still hex
-            //if so, they won't have a space
-            if (c.to_human().indexOf(" ") != -1) {
-                $("#ledger_cur_1").text(c.to_json());
-            } else {
-                $("#ledger_cur_1").text("Unknown currency format");
-            }
-        }
-    } else {
-        alert("Invalid hex code for currency");
+    if (!c.is_valid()) {
+        display_error("Invalid hex code for currency");
         return false;
+    } else if (c.is_native()) {
+        display_error("Error: XRP cannot have interest or demurrage.");
+        return false;
+    } else {
+        threeletters = c.get_iso();
+        $("#cur_1").val(threeletters);
+        
+        //Convert rate to percentage
+        rate = (c.get_interest_at(0) -1) * 100;
+        $("#rate_1").val(rate);
+        
+        //some currencies' to_human() output is still hex
+        //if so, they won't have a space
+        if (c.to_human().indexOf(" ") != -1) {
+            $("#ledger_cur_1").text(c.to_json());
+        } else {
+            $("#ledger_cur_1").text("Unknown currency format");
+        }
     }
 }
 
@@ -108,7 +111,8 @@ function get_ledger_amount() {
     var ledger_val = $("#ledger_val_1").val();
     var test = Amount.from_json(ledger_val+"/2/1");
     if (!test.is_valid()) {
-        alert("Error: Invalid ledger value");
+        display_error("Error: Invalid ledger value");
+        return false;
     }
     return test.to_text();//normalized string
 }
@@ -121,7 +125,8 @@ function get_display_amount() {
     var display_val = $("#display_val_1").val();
     var test = Amount.from_json(display_val+"/2/1");
     if (!test.is_valid()) {
-        alert("Error: Invalid display value");
+        display_error("Error: Invalid display value");
+        return false;
     }
     return test.to_text();
 }
@@ -131,19 +136,24 @@ function set_display_amount(value) {
 }
 
 function get_ref_time() {
-    t_string = $("#display_datetime_1").val();
-    t_date = new Date(t_string);
+    var t_string = $("#display_datetime_1").val();
+    var t_date = new Date(t_string);
     
     if (t_date === undefined) {
-        alert("Error: invalid reference time");
+        display_error("Error: invalid reference time");
+        return false;
     }
     
     return t_date;
 }
 
 function time_to_now() {
-    t = new Date();
+    var t = new Date();
     t.setMilliseconds(0);//Truncate ms to match Ripple convention
     $("#display_datetime_1").val( t.toISOString() );
+}
+
+function display_error(msg) {
+    alert(msg);
 }
 
